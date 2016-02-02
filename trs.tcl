acd257c31fce26a741fa16e61811d7165fb1dcda
mroll <mpr56@drexel.edu>
2016-02-01 22:13:55 -0500

    added some more rules

--- trs.tcl
+++ trs.tcl
@@ -8,6 +8,15 @@ proc forindex { i list body } {
     uplevel $script
 }
 
+proc unpack { vals args } {
+    set script {}
+    forindex i $vals {
+        lappend script [list set [lindex $args $i] [lindex $vals $i]]
+    }
+
+    uplevel [join $script "; "]
+}
+
 proc term? { expr } { expr { [llength $expr] > 1 } }
 
 proc reduce { term rules } {
@@ -50,6 +59,15 @@ proc operator? { term } {
     regexp [join [lmap op $operators {subst {^$op\$}}] |] $term
 }
 
+proc union { a b } {
+    set u {}
+    foreach el [concat $a $b] {
+        if { [lsearch $u $el] == -1 } { lappend u $el }
+    }
+
+    set u
+}
+
 proc regexp_cmd { redex contractum vars lvl } {
     set regex $redex
     set groups {}
@@ -62,7 +80,7 @@ proc regexp_cmd { redex contractum vars lvl } {
         } elseif { [term? $t] } {
             set drill [regexp_cmd $t $contractum $vars [expr {$lvl + 1}]]
             lset regex $i [lindex $drill 0]
-            set vars [concat $vars [lindex $drill 1]]
+            set vars [union $vars [lindex $drill 1]]
         } elseif { [regexp {^term_\d+} $t] } {
             lset regex $i %s
             lappend groups {({.*})}
@@ -103,9 +121,7 @@ proc mapper { varnames } {
 }
 
 proc rule { redex -> contractum } {
-    set transformer [regexp_cmd $redex $contractum {} 1]
-    set regex [lindex $transformer 0]
-    set vars  [lindex $transformer 1]
+    unpack [regexp_cmd $redex $contractum {} 1] regex vars
 
     return [subst { { term } {
                 set match \[regexp {$regex} \$term __r__ $vars\]
@@ -128,22 +144,28 @@ proc simplify { term rules } {
 proc add_rule { redex -> contractum } { uplevel [subst -nocommands { lappend rules [rule {$redex} -> {$contractum}] }] }
 
 set rules {}
-add_rule  {div {mul const_1 y} {mul const_2 z}} -> {mul {div const_1 const_2} {div y z}}
-add_rule  {div y y}                             -> 1
-add_rule  {add const_1 const_2}                 -> {[expr {1.0 * const_1 + const_2}]}
-add_rule  {mul const_1 const_2}                 -> {[expr {const_1 * const_2 * 1.0}]}
-add_rule  {pow const_1 const_2}                 -> {[expr {pow(const_1, const_2) * 1.0}]}
-add_rule  {deriv {pow y const_1}}               -> {mul const_1 {pow y [expr {const_1 - 1}]}}
-add_rule  {div const_1 const_2}                 -> {[expr {1.0 * const_1 / const_2}]}
-add_rule  {mul const_1 {mul const_2 term_1}}    -> {mul [expr {const_1 * const_2}] term_1}
-add_rule  {mul {pow y const_1} {pow y const_2}} -> {pow y [expr {const_1 + const_2}]}
-add_rule  {deriv {mul term_1 term_2}}           -> {add {mul {deriv term_1} term_2} {mul term_1 {deriv term_2}}}
-add_rule  {deriv {sin y}}                       -> {cos y}
-add_rule  {deriv {cos y}}                       -> {mul -1 {sin y}}
-add_rule  {sub const_1 const_2}                 -> {[expr {const_1 - const_2}]}
-add_rule  {mul y y}                             -> {pow y 2}
-add_rule  {mul {pow y const_1} y}               -> {pow y [expr {const_1 + 1}]}
-add_rule  {mul y {pow y const_1}}               -> {pow y [expr {const_1 + 1}]}
+add_rule {div {mul const_1 y} {mul const_2 z}} -> {mul {div const_1 const_2} {div y z}}
+add_rule {div y y}                             -> 1
+add_rule {add const_1 const_2}                 -> {[expr {1.0 * const_1 + const_2}]}
+add_rule {mul const_1 const_2}                 -> {[expr {const_1 * const_2 * 1.0}]}
+add_rule {pow const_1 const_2}                 -> {[expr {pow(const_1, const_2) * 1.0}]}
+add_rule {deriv {pow y const_1}}               -> {mul const_1 {pow y [expr {const_1 - 1}]}}
+add_rule {div const_1 const_2}                 -> {[expr {1.0 * const_1 / const_2}]}
+add_rule {mul const_1 {mul const_2 term_1}}    -> {mul [expr {const_1 * const_2}] term_1}
+add_rule {mul {pow y const_1} {pow y const_2}} -> {pow y [expr {const_1 + const_2}]}
+add_rule {deriv {mul term_1 term_2}}           -> {add {mul {deriv term_1} term_2} {mul term_1 {deriv term_2}}}
+add_rule {deriv {sin y}}                       -> {cos y}
+add_rule {deriv {cos y}}                       -> {mul -1 {sin y}}
+add_rule {sub const_1 const_2}                 -> {[expr {const_1 - const_2}]}
+add_rule {mul y y}                             -> {pow y 2}
+add_rule {mul {pow y const_1} y}               -> {pow y [expr {const_1 + 1}]}
+add_rule {mul y {pow y const_1}}               -> {pow y [expr {const_1 + 1}]}
+add_rule {mul {mul y z} y}                     -> {mul {pow y 2} z}
+add_rule {mul z {mul y z}}                     -> {mul {pow y 2} z}
+add_rule {mul z {mul term_1 z}}                -> {mul {pow z 2} term_1}
+add_rule {mul {mul term_1 z} z}                -> {mul {pow z 2} term_1}
+add_rule {mul {mul const_1 y} y}               -> {mul {pow y 2} const_1}
+add_rule {mul {pow y const_1} {mul const_2 y}} -> {mul {pow y [expr {const_1 + 1.0}]} const_2}
 
 proc prompt { msg } {
     puts -nonewline $msg
@@ -164,10 +186,14 @@ proc interact { } {
     set input [prompt "> "]
     while { ! [string equal $input quit] } {
         set term [expression::parse $input $::tokens $expression::optable ident]
-        if { [llength $term] } { puts "expression tree: $term" }
-        if { [valid $term] }   { puts [simplify $term $::rules] }
+        if { [valid $term] } {
+            puts "expression tree: $term"
+            puts [simplify $term $::rules]
+        }
         set input [prompt "> "]
     }
 }
 
-interact
+simplify {deriv {sin matt}} $rules
+
+# interact
